8eb3794d8577c61bcc988698686e1398c6e566ba
seohyunCho <shanny06@naver.com>
2023-10-16 14:53:45 +0900

    프레임 사이즈 설정

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame();
+        frame.setTitle("완벽한 학급 만들기");
+        // Get the screen size
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+        // Set the frame size to match the screen size
+        frame.setSize(screenSize);
+
+        // Set the frame's content pane to your GUI
+        // frame.getContentPane().add(yourGUIComponent);
+
+        // Set the frame to be visible
+        frame.setVisible(true);
+
+        // Exit the program when the frame is closed
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
     }
 }
