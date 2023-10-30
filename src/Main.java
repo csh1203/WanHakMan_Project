50b759564d6668afa46fb8327fce613e08e9f172
seohyunCho <shanny06@naver.com>
2023-10-30 14:36:58 +0900

    메인 화면 구성 및 각 페이지로 연결

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Main {
+public class Main extends Frame {
     public static void main(String[] args) {
-        JFrame frame = new JFrame();
-        frame.setTitle("완벽한 학급 만들기");
-        // Get the screen size
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-        // Set the frame size to match the screen size
-        frame.setSize(screenSize);
-
-        // Set the frame's content pane to your GUI
-        // frame.getContentPane().add(yourGUIComponent);
-
-        // Set the frame to be visible
-        frame.setVisible(true);
-
-        // Exit the program when the frame is closed
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
+       new mainScreen();
     }
 }
